a4cd52bfd6f635727a6de5a2f5ab3082b04f3b95
Josh Bolling <josh@Joshs-MacBook-Air.local>
2021-08-23 08:50:50 -0400

    Added db_subnet_group_name parameter and aws_db_subnet_group resource

--- lamp-stack-ansible/rds.tf
+++ lamp-stack-ansible/rds.tf
@@ -1,13 +1,24 @@
+resource "aws_db_subnet_group" "default" {
+    name       = "db_main"
+    subnet_ids = [aws_subnet.bn_test_private_subnet_01.id]
+
+    tags = {
+
+        name = "Main DB subnet group"
+    }
+}
+
 resource "aws_db_instance" "bn_test_mysql_db" {
 
-    allocated_storage      = 5
-    engine                 = "mysql"
-    engine_version         = "5.7"
-    instance_class         = "db.t2.micro"
-    name                   = "wordpress"
-    username               = var.db_username
-    password               = var.db_password
-    vpc_security_group_ids = [aws_security_group.bn_test_sg_3306_from_private_to_rds.id]
-    skip_final_snapshot    = true
+    allocated_storage        = 5
+    engine                   = "mysql"
+    engine_version           = "5.7"
+    instance_class           = "db.t2.micro"
+    name                     = "wordpress"
+    username                 = var.db_username
+    password                 = var.db_password
+    db_subnet_group_name     = aws_db_subnet_group.db_main.name
+    vpc_security_group_ids   = [aws_security_group.bn_test_sg_3306_from_private_to_rds.id]
+    skip_final_snapshot      = true
 
 }
