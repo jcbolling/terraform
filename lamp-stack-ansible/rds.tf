5406afb56e56af82e27250975f3797621e6f03f7
Josh Bolling <josh@Joshs-MacBook-Air.local>
2021-08-23 08:32:07 -0400

    Added vpc_security_group_ids

--- lamp-stack-ansible/rds.tf
+++ lamp-stack-ansible/rds.tf
@@ -1,12 +1,13 @@
 resource "aws_db_instance" "bn_test_mysql_db" {
 
-    allocated_storage   = 5
-    engine              = "mysql"
-    engine_version      = "5.7"
-    instance_calss      = "db.t2.micro"
-    name                = "wordpress"
-    username            = var.db_username
-    password            = var.db_password
-    skip_final_snapshot = true
-    
+    allocated_storage      = 5
+    engine                 = "mysql"
+    engine_version         = "5.7"
+    instance_calss         = "db.t2.micro"
+    name                   = "wordpress"
+    username               = var.db_username
+    password               = var.db_password
+    vpc_security_group_ids = [aws_security_group.bn_test_sg_3306_from_private_to_rds.id]
+    skip_final_snapshot    = true
+
 }
