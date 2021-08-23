f264c49a1ff8d03335cb04f86cda41fcdd498636
Josh Bolling <josh@Joshs-MacBook-Air.local>
2021-08-23 08:34:45 -0400

    Fixed typo

--- lamp-stack-ansible/rds.tf
+++ lamp-stack-ansible/rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "bn_test_mysql_db" {
     allocated_storage      = 5
     engine                 = "mysql"
     engine_version         = "5.7"
-    instance_calss         = "db.t2.micro"
+    instance_class         = "db.t2.micro"
     name                   = "wordpress"
     username               = var.db_username
     password               = var.db_password
